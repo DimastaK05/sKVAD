7de28b030a22eeced027f709b0e7ba6767901f8c
DimastaK05 <146315836+DimastaK05@users.noreply.github.com>
2025-05-27 21:31:20 +0300

    создал гем x2

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in symbol_diff_polynomials_brackets.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/symbol_diff_polynomials_brackets.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "symbol_diff_polynomials_brackets/version"
+
+module SymbolDiffPolynomialsBrackets
+  class Error < StandardError; end
+  # Your code goes here...
+end
